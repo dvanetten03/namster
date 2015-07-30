e294c4ec1aa218fd5d8ed789b44354066f8b3fc3
Diane Van Etten <diane.vanetten@me.com>
2015-07-30 20:48:46 +0000

    Added user authentication to the application by installing the devise gem

--- Gemfile
+++ Gemfile
@@ -6,6 +6,9 @@ gem 'rails', '4.0.1'
 # Use postgresql as the database for Active Record
 gem 'pg'
 
+# Adding gem for user authentication
+gem 'devise', '>= 3.2.4'
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Namster::Application.configure do
+  config.action_mailer.default_url_options = { :host => 'namster-diane-van-etten.herokuapp.com'}
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
