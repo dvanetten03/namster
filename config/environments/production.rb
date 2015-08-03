ae976ed608be758d67916c1aefe9c6ca78a8490a
Diane Van Etten <diane.vanetten@me.com>
2015-08-03 02:30:41 +0000

    changed production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Namster::Application.configure do
 
-  ActionMailer::Base.smtp_settings = {
+  ActionMailer::Base.smtp_settings = 
+    config.action_mailer.default_url_options = { :host => namster-diane-van-etten.herokuapp.com }
+  {
     :port         => ENV['MAILGUN_SMTP_PORT'],
     :address      => ENV['MAILGUN_SMTP_SERVER'],
     :user_name    => ENV['MAILGUN_SMTP_LOGIN'],
